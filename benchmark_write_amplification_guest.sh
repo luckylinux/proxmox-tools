51b83fea619d77dc3020a402b0a6cf20d8a0e9ed
luckylinux <luckylinux>
2025-02-01 15:39:50 +0100

    Initial Version of Benchmark Script.

--- benchmark_write_amplification_guest.sh
+++ benchmark_write_amplification_guest.sh
@@ -21,23 +21,31 @@ run_command_inside_vm() {
 # Random IO Test Function
 random_io() {
     # Input Arguments
+    local lbs=${1-"${BENCHMARK_VM_DEFAULT_RANDOM_BLOCK_SIZE}"}
+    local lsize=${2-"${BENCHMARK_VM_DEFAULT_SIZE}"}
 
-    # Run Test
-    sudo fio --name=write_iops --directory="${BENCHMARK_VM_TEST_PATH}" --size=10G \
+    # Test Command
+    echo <<- EOF
+    sudo fio --name=write_iops --directory="${BENCHMARK_VM_TEST_PATH}" --size="${lsize}" \
     --time_based --runtime=600s --ramp_time=2s --ioengine=libaio --direct=1 \
-    --verify=0 --bs=4K --iodepth=64 --rw=randwrite --group_reporting=1
+    --verify=0 --bs="${lbs}" --iodepth=64 --rw=randwrite --group_reporting=1
+    EOF
 }
 
 # Throuput Test Function
 throuput_io() {
     # Input Arguments
+    local lbs=${1-"${BENCHMARK_VM_DEFAULT_RANDOM_BLOCK_SIZE}"}
+    local lsize=${2-"${BENCHMARK_VM_DEFAULT_SIZE}"}
 
-
-    # Run Test
+    # Test Command
+    echo <<- EOF
     sudo fio --name=write_throughput --directory="${BENCHMARK_VM_TEST_PATH}" --numjobs=16 \
-    --size=10G --time_based --runtime=60s --ramp_time=2s --ioengine=libaio \
-    --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write \
+    --size="${lsize}" --time_based --runtime=60s --ramp_time=2s --ioengine=libaio \
+    --direct=1 --verify=0 --bs="${lbs}" --iodepth=64 --rw=write \
     --group_reporting=1
+    EOF
 }
 
 # Run Several Tests
+run_command_inside_vm $(random_io)
